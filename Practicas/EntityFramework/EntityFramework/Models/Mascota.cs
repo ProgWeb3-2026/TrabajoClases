1a5f0f6d10d0f6754e2fcdbee19b329a55881545
Victor Ortega <vortegal@univalle.edu>
2026-09-03 15:03:34 -0400

    Base Clase EF

---
+++ Practicas/EntityFramework/EntityFramework/Models/Mascota.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+
+
+        [Required]
+        public string Raza { get; set; }
+
+        [Range(0, 100)]
+        public int Edad { get; set; }
+
+        public decimal Peso { get; set; }
+
+        [Required]
+        public bool EsDomestica { get; set; }
+    }
+}
