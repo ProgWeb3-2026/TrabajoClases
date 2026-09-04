d12d73731afb975e1260678bfcf0664430055f86
Victor Ortega <vortegal@univalle.edu>
2026-09-03 21:37:14 -0400

    Modulos mascotas y hoteles

---
+++ Practicas/EntityFramework/EntityFramework/EntityFrameworkContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+public class EntityFrameworkContext(DbContextOptions<EntityFrameworkContext> options) : 
+    DbContext(options)
+{
+    public DbSet<EntityFramework.Models.Mascota> Mascota { get; set; } = default!;
+
+    public DbSet<EntityFramework.Models.Hotel> Hotel { get; set; } = default!;
+
+}
