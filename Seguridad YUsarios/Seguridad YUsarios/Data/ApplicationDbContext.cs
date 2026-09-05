619cc21c82ffdc7a99316ced7ad36661d6e08fbf
c4yalo <cayalomaicol7@gmail.com>
2026-09-04 21:27:48 -0400

    Practica paginas

---
+++ Seguridad YUsarios/Seguridad YUsarios/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Seguridad_YUsarios.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        
+    }
+}
