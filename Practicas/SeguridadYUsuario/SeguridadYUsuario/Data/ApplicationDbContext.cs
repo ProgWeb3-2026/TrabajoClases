df72097e5c91370ab2bf64801f9e0661efe9ec75
Luis Polo <pololuisangel0@gmail.com>
2026-09-04 21:19:00 -0400

    commit Login de usuario Polo

---
+++ Practicas/SeguridadYUsuario/SeguridadYUsuario/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SeguridadYUsuario.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
