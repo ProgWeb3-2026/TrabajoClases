d57a7b9218a396f77dfaa277f8a2718624bba1c9
Deivis Esteban Callisaya Mollericona <deivisestebancallisayamolleric@gmail.com>
2026-09-04 21:27:31 -0400

    Login y Register_DeivisCallisaya

---
+++ Practicas/SeguridadYUsuarios/SeguridadYUsuarios/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SeguridadYUsuarios.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
